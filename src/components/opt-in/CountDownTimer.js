e5f3dfc80e8135a6eb24c23dca887d3660dbe84e
Luke <lukerbab@gmail.com>
2019-09-13 19:15:34 +0100

    feature: added some common styles for flex box

--- src/components/opt-in/CountDownTimer.js
+++ src/components/opt-in/CountDownTimer.js
@@ -12,23 +12,25 @@ import {
 
 export default function CountDownTimer() {
 	return (
-		<CountDownTimerContainerStyled>
+		<CountDownTimerContainerStyled className="u-flex-column u-align-center">
 
-			<CountDownTimerCashValueContainerStyled>
-				<CountDownTimerCashValueImageStyled>
-				</CountDownTimerCashValueImageStyled>
+			<CountDownTimerCashValueContainerStyled className="u-flex-column u-align-center">
+				<CountDownTimerCashValueImageStyled src="https://www.starspins.com/api/content/offer/campaign/welcome-bundle/__assets/styles/images/starspins/select/scale-2/box-bottom.png" />
 
 				<CountDownCashValueMessageStyled>
+					This is the Cash Value Message
 				</CountDownCashValueMessageStyled>
 			</CountDownTimerCashValueContainerStyled>
 
 			<CountDownTimerClockContainerStyled>
 				<CountDownTimerValueStyled>
+					00 : 00 : 00
 				</CountDownTimerValueStyled>
 			</CountDownTimerClockContainerStyled>
 
 			<CountDownTimerButtonContainerStyled>
 				<CountDownTimerButtonStyled>
+
 				</CountDownTimerButtonStyled>
 			</CountDownTimerButtonContainerStyled>
 
