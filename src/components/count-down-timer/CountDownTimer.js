6aa555b24bc6a5ec42cde33b8fc82e894829dadb
Luke <lukerbab@gmail.com>
2019-09-14 22:39:33 +0100

    feature: only rendering timer if time is 0

--- src/components/count-down-timer/CountDownTimer.js
+++ src/components/count-down-timer/CountDownTimer.js
@@ -15,6 +15,7 @@ export default function CountDownTimer(props) {
 	});
 	const [optInLink, setOptInLink] = useState('');
 	const [duration, setDuration] = useState(0);
+	const [timerComplete, setTimerComplete] = useState(false);
 
 	const _setDataFromConfig = useCallback(() => {
 		if (props.config) {
@@ -28,12 +29,30 @@ export default function CountDownTimer(props) {
 		_setDataFromConfig();
 	}, [props.config, _setDataFromConfig]);
 
+	function updateTimerCompleteStatus(value) {
+		setTimerComplete(value);
+	}
+
+	function showClock() {
+		if (timerComplete) {
+			return (
+				<>
+					<CashValue cashValue={cashValue}/>
+					<Timer duration={duration} updateTimerCompleteStatus={updateTimerCompleteStatus}/>
+					<OptIn link={optInLink}/>
+				</>
+			)
+		} else {
+			return (
+				<Timer duration={duration} updateTimerCompleteStatus={updateTimerCompleteStatus}/>
+			);
+		}
+	}
+
 	if (props.config) {
 		return (
 			<CountDownTimerContainerStyled className="u-flex-column u-align-center">
-				<CashValue cashValue={cashValue}/>
-				<Timer duration={duration}/>
-				<OptIn link={optInLink}/>
+				{showClock()}
 			</CountDownTimerContainerStyled>
 		);
 	} else {
