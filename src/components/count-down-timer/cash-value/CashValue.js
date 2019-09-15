77be9b88c106ed815ce4992ef8fe9f2d99d296b5
Luke <lukerbab@gmail.com>
2019-09-15 14:10:55 +0100

    feature: added responsive image styling for header image

--- src/components/count-down-timer/cash-value/CashValue.js
+++ src/components/count-down-timer/cash-value/CashValue.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React             from 'react';
+import { useMediaQuery } from 'react-responsive';
 import {
 	CountDownCashValueMessageStyled,
 	CountDownTimerCashValueContainerStyled,
 	CountDownTimerCashValueImageStyled
-}            from '../CountDownTimer.styles';
+}                        from '../CountDownTimer.styles';
 
+import { maxWidthSM } from '../../../styles/media-queries.styles';
 
 export default function CashValue(props) {
+	const largeImage = 'https://www.starspins.com/api/content/offer/campaign/welcome-bundle/__assets/styles/images/starspins/select/scale-2/box-bottom.png';
+	const smallImage = 'https://www.starspins.com/api/content/offer/campaign/welcome-bundle/__assets/styles/images/starspins/select/scale-1/box-bottom.png';
+	const matches = useMediaQuery({query: `(${maxWidthSM})`});
+
+	function _displayHeaderImage() {
+		if (matches) {
+			return (
+				<CountDownTimerCashValueImageStyled src={smallImage}/>
+			)
+		} else {
+			return (
+				<CountDownTimerCashValueImageStyled src={largeImage}/>
+			)
+		}
+	}
+
 	return (
 		<CountDownTimerCashValueContainerStyled className="u-flex-column u-align-center">
-			<CountDownTimerCashValueImageStyled
-				src="https://www.starspins.com/api/content/offer/campaign/welcome-bundle/__assets/styles/images/starspins/select/scale-2/box-bottom.png"/>
+			{_displayHeaderImage()}
 
 			<CountDownCashValueMessageStyled>
 				Get your free {props.cashValue.currency}{props.cashValue.value} now
